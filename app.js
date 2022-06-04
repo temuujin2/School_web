21efcf9ee1bca956c75d6ddaada172d69dee2d66
Temuujin Erdenebileg <102133245+temuujin2@users.noreply.github.com>
2022-06-04 23:59:23 +0800

    june 4 23:59
    Update

--- app.js
+++ app.js
@@ -43,3 +43,63 @@ $("figure").mouseleave(
       $(this).removeClass("hover");
     }
   );
+
+
+//   ======================================
+
+/*------------Validation Function-----------------*/
+var count = 0; // To count blank fields.
+function validation(event) {
+var radio_check = document.getElementsByClassName('checkbox'); // Fetching radio button by name.
+var input_field = document.getElementsByClassName('text_field'); // Fetching all inputs with same class name text_field and an html tag textarea.
+var text_area = document.getElementsByTagName('textarea');
+// Validating radio button.
+if (radio_check[0].checked == false && radio_check[1].checked == false) {
+var y = 0;
+} else {
+var y = 1;
+}
+// For loop to count blank inputs.
+for (var i = input_field.length; i > count; i--) {
+if (input_field[i - 1].value == '' || text_area.value == '') {
+count = count + 1;
+} else {
+count = 0;
+}
+}
+if (count != 0 || y == 0) {
+    confirm("Мэдээлэл илгээх үү?");  // Notifying validation
+event.preventDefault();
+} else {
+return true;
+}
+}
+/*---------------------------------------------------------*/
+// Function that executes on click of first next button.
+function next_step1() {
+document.getElementById("first").style.display = "none";
+document.getElementById("second").style.display = "block";
+document.getElementById("active2").style.background = "#f27334";
+}
+// Function that executes on click of first previous button.
+function prev_step1() {
+document.getElementById("first").style.display = "block";
+document.getElementById("second").style.display = "none";
+document.getElementById("active1").style.background = "#f27334";
+document.getElementById("active2").style.background = "rgb(207, 207, 207)";
+}
+// Function that executes on click of second next button.
+function next_step2() {
+document.getElementById("second").style.display = "none";
+document.getElementById("third").style.display = "block";
+document.getElementById("active3").style.background = "#f27334";
+}
+// Function that executes on click of second previous button.
+function prev_step2() {
+document.getElementById("third").style.display = "none";
+document.getElementById("second").style.display = "block";
+document.getElementById("active2").style.background = "#f27334";
+document.getElementById("active3").style.background = "rgb(207, 207, 207)";
+}
+
+// ===================== alert box ==================
